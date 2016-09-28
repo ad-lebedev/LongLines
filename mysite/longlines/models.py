7f057305e881dede14ddaf64d8aa754048490084
Kostya Zi <kzinatulin@gmail.com>
2016-09-28 14:05:01 +0300

    some mistakes in models is fixed

--- mysite/longlines/models.py
+++ mysite/longlines/models.py
@@ -17,25 +17,27 @@ class task(ModelAudit):
     description = models.CharField(max_length=1000)
 
 class task_list(ModelAudit):
-    task = models.ManyToManyField(
+    tasks = models.ManyToManyField(
         task,
         through='task_task_list',
-        through_fields=('task','task_list')
+        through_fields=('task_list', 'task')
     )
 
 class task_task_list(models.Model):
-    task = models.ForeignKey(task,on_delete=models.DO_NOTHING())
-    task_list = models.ForeignKey(task_list, on_delete=models.DO_NOTHING())
+    task_list = models.ForeignKey(task_list, on_delete=models.CASCADE)
+    task = models.ForeignKey(task, on_delete=models.CASCADE)
     task_number = models.PositiveSmallIntegerField()
 
 class learning_group(models.Model):
     name = models.CharField(max_length=10)
     date_started = models.DateField()
-    task_list = models.ForeignKey(task,on_delete=models.DO_NOTHING())
+    task_list = models.ForeignKey(task, on_delete=models.CASCADE)
     tutor = models.ForeignKey(
         User,
-        on_delete=models.DO_NOTHING(),
-        limit_choices_to={'is_active':True})
+        on_delete=models.CASCADE,
+        related_name='tutors',
+        related_query_name='tutor',
+        limit_choices_to={'is_active': True})
     student = models.ManyToManyField(
         User,
         through='learning_group_members',
@@ -43,50 +45,47 @@ class learning_group(models.Model):
     )
 
 class learning_group_members(models.Model):
-    group = models.ForeignKey(learning_group, on_delete=models.DO_NOTHING())
-    student = models.ForeignKey(User,on_delete=models.DO_NOTHING(),unique=True)
+    group = models.ForeignKey(learning_group, on_delete=models.CASCADE)
+    student = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
 
 class task_progress(models.Model):
     status_choices = (
-        (1,'New'),
-        (2,'Sent to check'),
-        (3,'Passed'),
-        (4,'Declined')
+        (1, 'New'),
+        (2, 'Sent to check'),
+        (3, 'Passed'),
+        (4, 'Declined')
     )
-    task = models.ForeignKey(task, on_delete=models.CASCADE())
-    student = models.ForeignKey(User,on_delete=models.CASCADE())
-    task_status = models.PositiveSmallIntegerField(verbose_name='status',choices=status_choices)
+    task = models.ForeignKey(task, on_delete=models.CASCADE)
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    task_status = models.PositiveSmallIntegerField(verbose_name='status', choices=status_choices)
     created_date = models.DateTimeField()
     changed_date = models.DateTimeField()
 
 class parameters(models.Model):
     diargam_choices = (
-        (1,'Open circuit'),
-        (2,'Short circuit'),
-        (3,'Resistance'),
-        (4,'Capacitance'),
-        (5,'Parallel res/cap'),
-        (6,'Series res/cap'),
-        (7,'Inductance'),
-        (8,'Parallel res/ind'),
-        (9,'Series res/ind'),
+        (1, 'Open circuit'),
+        (2, 'Short circuit'),
+        (3, 'Resistance'),
+        (4, 'Capacitance'),
+        (5, 'Parallel res/cap'),
+        (6, 'Series res/cap'),
+        (7, 'Inductance'),
+        (8, 'Parallel res/ind'),
+        (9, 'Series res/ind'),
     )
-    task = models.ForeignKey(task_progress, on_delete=models.CASCADE())
+    task = models.ForeignKey(task_progress, on_delete=models.CASCADE)
     date_published = models.DateTimeField()
     circuit_diagram_type = models.PositiveSmallIntegerField(choices=diargam_choices)
-    R_gen = models.PositiveSmallIntegerField(max_length=3)
-    U_gen = models.PositiveSmallIntegerField(max_length=4,default=1000)
-    T_gen = models.PositiveSmallIntegerField(verbose_name='pulse time',max_length=3)
-    L_line = models.PositiveSmallIntegerField(verbose_name='length',max_length=2)
-    X_line = models.PositiveSmallIntegerField(verbose_name='position',max_length=2)
-    T_line = models.PositiveSmallIntegerField(max_length=3)
-    Ro_line = models.PositiveSmallIntegerField(max_length=3)
-    R_load = models.PositiveSmallIntegerField(max_length=3)
-    L_load = models.PositiveSmallIntegerField(max_length=3)
-    C_load = models.PositiveSmallIntegerField(max_length=3)
-    T_delay_osc = models.PositiveSmallIntegerField(verbose_name='delay',max_length=3)
-    T_sweep_osc = models.PositiveSmallIntegerField(verbose_name='sweep',max_length=3)
+    R_gen = models.PositiveSmallIntegerField()
+    U_gen = models.PositiveSmallIntegerField(default=1000)
+    T_gen = models.PositiveSmallIntegerField(verbose_name='pulse time')
+    L_line = models.PositiveSmallIntegerField(verbose_name='length')
+    X_line = models.PositiveSmallIntegerField(verbose_name='position')
+    T_line = models.PositiveSmallIntegerField()
+    Ro_line = models.PositiveSmallIntegerField()
+    R_load = models.PositiveSmallIntegerField()
+    L_load = models.PositiveSmallIntegerField()
+    C_load = models.PositiveSmallIntegerField()
+    T_delay_osc = models.PositiveSmallIntegerField(verbose_name='delay')
+    T_sweep_osc = models.PositiveSmallIntegerField(verbose_name='sweep')
     comment = models.CharField(max_length=500)
-
-
-
